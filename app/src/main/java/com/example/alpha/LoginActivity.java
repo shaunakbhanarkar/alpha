648033852086c4f542578a397b4d3e25ee134530
shaunakbhanarkar <shaunak.bhanarkar@gmail.com>
2020-03-09 17:05:36 +0530

    firebase realtime database integration added

--- app/src/main/java/com/example/alpha/LoginActivity.java
+++ app/src/main/java/com/example/alpha/LoginActivity.java
@@ -16,6 +16,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Objects;
 
@@ -89,6 +91,11 @@ public class LoginActivity extends AppCompatActivity {
             GoogleSignInAccount account = task.getResult();
             Log.e("try block","entered");
             if(account != null){
+
+                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+                databaseReference.child("Users").child(account.getId()).child("Name").setValue(account.getDisplayName());
+                databaseReference.child("Users").child(account.getId()).child("Email").setValue(account.getEmail());
+
                 finishAffinity();
                 Intent intent = new Intent(this,MainActivity.class);
                 startActivity(intent);
